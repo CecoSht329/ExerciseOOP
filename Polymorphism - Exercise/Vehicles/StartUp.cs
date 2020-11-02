30aaaacfab55772758bb18496bcbb9f28cf45c01
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-02 17:41:47 +0200

    Create StartUp.cs

---
+++ Polymorphism - Exercise/Vehicles/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Vehicles
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var carInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Car car = new Car(double.Parse(carInfo[1]), double.Parse(carInfo[2]));
+
+            var truckInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Truck truck = new Truck(double.Parse(truckInfo[1]), double.Parse(truckInfo[2]));
+
+            var numberOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                var command = Console.ReadLine();
+                if (command.StartsWith("Drive Car"))
+                {
+                    car.Drive(double.Parse(command.Split(" ", StringSplitOptions.RemoveEmptyEntries)[2]));
+                }
+                else if (command.StartsWith("Drive Truck"))
+                {
+                    truck.Drive(double.Parse(command.Split(" ", StringSplitOptions.RemoveEmptyEntries)[2]));
+                }
+                else if (command.StartsWith("Refuel Car"))
+                {
+                    car.Refuel(double.Parse(command.Split(" ", StringSplitOptions.RemoveEmptyEntries)[2]));
+                }
+                else if (command.StartsWith("Refuel Truck"))
+                {
+                    truck.Refuel(double.Parse(command.Split(" ", StringSplitOptions.RemoveEmptyEntries)[2]));
+                }
+            }
+            Console.WriteLine($"Car: {car.FuelQuantity:f2}");
+            Console.WriteLine($"Truck: {truck.FuelQuantity:f2}");
+        }
+    }
+}
