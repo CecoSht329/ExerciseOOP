fd3356f0c965266b82056c4ef1d7dc7ab77ca145
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-02 15:29:24 +0200

    Create AddRemoveCollection.cs

---
+++ CollectionHierarchy/Models/AddRemoveCollection.cs
@@ -0,0 +1,30 @@
+﻿
+using CollectionHierarchy.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CollectionHierarchy
+{
+    public class AddRemoveCollection : IAddRemoveCollection
+    {
+        public AddRemoveCollection()
+        {
+            this.Collection = new List<string>();
+        }
+        public List<string> Collection { get; }
+
+        public string Add(string item)
+        {
+            this.Collection.Insert(0, item);
+            return "0";
+        }
+
+        public string Remove()
+        {
+            var lastItem = this.Collection.Last();
+            this.Collection.Remove(lastItem);
+
+            return lastItem;
+        }
+    }
+}
