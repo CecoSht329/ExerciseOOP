87b35f5cbdb8ec2bcda03016ff9bc2cd8a6f3a6c
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-02 15:27:54 +0200

    Update Spy.cs

--- MilitaryElite/Models/Spy.cs
+++ MilitaryElite/Models/Spy.cs
@@ -1,24 +1,31 @@
-﻿
-using System.Text;
+﻿using System.Text;
 
 namespace MilitaryElite
 {
-    public class Spy : Soldier, ISpy
+    public class Spy : ISoldier, ISpy
     {
-        //•	Spy: "Spy <id> <firstName> <lastName> <codeNumber>"
-        public Spy(string id, string firstName, string lastName, int codeNumber)
-            : base(id, firstName, lastName)
+        public Spy(int id, string firstName, string lastName, int codeNumber)
         {
-            CodeNumber = codeNumber;
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.CodeNumber = codeNumber;
         }
 
-        public int CodeNumber { get; private set; }
+        public int Id { get; }
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+
+        public int CodeNumber { get; }
 
         public override string ToString()
         {
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine($"Name: {FirstName} {LastName} Id: {Id}");
-            sb.AppendLine($"Code Number: {CodeNumber}");
+            var sb = new StringBuilder();
+            sb.AppendLine($"Name: {this.FirstName} {this.LastName} Id: {this.Id}")
+                .AppendLine($"Code Number: {this.CodeNumber}");
+
             return sb.ToString().Trim();
         }
     }
