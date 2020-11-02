d62f97966326305084e53667d182b167249ebe52
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-02 15:29:20 +0200

    Create AddCollection.cs

---
+++ CollectionHierarchy/Models/AddCollection.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections.Generic;
+
+namespace CollectionHierarchy
+{
+    public class AddCollection : IAddCollection
+    {
+        public AddCollection()
+        {
+            this.Collection = new List<string>();
+        }
+
+        public List<string> Collection { get; }
+
+        public string Add(string item)
+        {
+            this.Collection.Add(item);
+            return (Collection.Count-1).ToString();
+        }
+    }
+}
