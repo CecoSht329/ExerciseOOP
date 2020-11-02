3082164021174e18601b47be69571298b140f059
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-02 15:29:26 +0200

    Create MyList.cs

---
+++ CollectionHierarchy/Models/MyList.cs
@@ -0,0 +1,33 @@
+﻿
+using CollectionHierarchy.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CollectionHierarchy
+{
+    public class MyList : IMyList
+    {
+        public MyList()
+        {
+            Collection = new List<string>();
+        }
+        public List<string> Collection { get; }
+
+        public int Used
+            => this.Collection.Count;
+
+        public string Add(string item)
+        {
+            this.Collection.Insert(0, item);
+            return "0";
+        }
+
+        public string Remove()
+        {
+            var firstElement = this.Collection.First();
+            this.Collection.Remove(firstElement);
+
+            return firstElement;
+        }
+    }
+}
