8dc50a50ae5a2919e01a35f40df0a84e76a92049
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-02 17:41:22 +0200

    Create Truck.cs

---
+++ Polymorphism - Exercise/Vehicles/Models/Truck.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+
+namespace Vehicles
+{
+    public class Truck : IVehicle
+    {
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public Truck(double fuelQuantity, double fuelConsumption)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public double FuelQuantity
+        {
+            get
+            {
+                return fuelQuantity;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException();
+                }
+                fuelQuantity = value;
+            }
+        }
+        public double FuelConsumption
+        {
+            get
+            {
+                return fuelConsumption;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException();
+                }
+                fuelConsumption = value;
+            }
+        }
+
+
+        public void Drive(double distance)
+        {
+            if (FuelQuantity >= distance * (FuelConsumption + CommonValues.ConsumptionIncreaseForTruck))
+            {
+                FuelQuantity -= distance * (FuelConsumption + CommonValues.ConsumptionIncreaseForTruck);
+                Console.WriteLine($"Truck travelled {distance} km");
+            }
+            else
+            {
+                Console.WriteLine("Truck needs refueling");
+            }
+
+        }
+
+        public void Refuel(double fuel)
+        {
+            FuelQuantity += (fuel * 0.95);
+        }
+    }
+}
