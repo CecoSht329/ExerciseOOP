d35e292fcbfb8bac1de895ca908330b5d4552937
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-02 17:41:24 +0200

    Create Car.cs

---
+++ Polymorphism - Exercise/Vehicles/Models/Car.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+
+namespace Vehicles
+{
+    public class Car : IVehicle
+    {
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public Car(double fuelQuantity, double fuelConsumption)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public double FuelQuantity
+        {
+            get
+            {
+                return fuelQuantity;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException();
+                }
+                fuelQuantity = value;
+            }
+        }
+
+        public double FuelConsumption
+        {
+            get
+            {
+                return fuelConsumption;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException();
+                }
+                fuelConsumption = value;
+            }
+        }
+
+        public void Drive(double distance)
+        {
+            if (FuelQuantity >= distance * (FuelConsumption + CommonValues.ConsumptionIncreaseForCar))
+            {
+                FuelQuantity -= distance * (FuelConsumption + CommonValues.ConsumptionIncreaseForCar);
+                Console.WriteLine($"Car travelled {distance} km");
+            }
+            else
+            {
+                Console.WriteLine("Car needs refueling");
+            }
+
+        }
+
+        public void Refuel(double fuel)
+        {
+            FuelQuantity += fuel;
+        }
+    }
+}
