25c36b7940e225ae7128a1be7e27580852d68482
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-02 15:27:25 +0200

    Update ISoldier.cs

--- MilitaryElite/Contracts/ISoldier.cs
+++ MilitaryElite/Contracts/ISoldier.cs
@@ -3,8 +3,7 @@ namespace MilitaryElite
 {
     public interface ISoldier
     {
-        //•	Soldier - general class for Soldiers, holding id, first name and last name.
-        public string Id { get; }
+        public int Id { get; }
         public string FirstName { get; }
         public string LastName { get; }
     }
