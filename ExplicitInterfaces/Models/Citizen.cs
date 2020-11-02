8876f63b8d1d9585061e697bd84961dbd202262b
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-02 15:30:32 +0200

    Create Citizen.cs

---
+++ ExplicitInterfaces/Models/Citizen.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ExplicitInterfaces
+{
+    class Citizen : IPerson, IResident
+    {
+        public Citizen(string name, string age, string country)
+        {
+            Name = name;
+            Age = age;
+            Country = country;
+        }
+
+        public string Name { get; private set; }
+        public string Country { get; private set; }
+        public string Age { get; private set; }
+
+    }
+}
