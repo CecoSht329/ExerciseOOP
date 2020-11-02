88d54bf9f75c1294bf747bc6a65a8d99ec105f43
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-02 15:30:17 +0200

    Create IPerson.cs

---
+++ ExplicitInterfaces/Contracts/IPerson.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ExplicitInterfaces
+{
+    public interface IPerson
+    {
+        //name, an age and a method GetName(). 
+        public string Name { get; }
+        public string Age { get; }
+
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+}
