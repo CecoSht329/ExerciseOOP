df299a7dd5fbe2e6d1dfeb5807703806639c237d
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-02 15:29:13 +0200

    Create IAddCollection.cs

---
+++ CollectionHierarchy/Contracts/IAddCollection.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.Generic;
+
+namespace CollectionHierarchy
+{
+    public interface IAddCollection
+    {
+        List<string> Collection { get; }
+
+        public string Add(string item);
+    }
+}
