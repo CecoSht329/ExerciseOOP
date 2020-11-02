cfa42c9c803fca3a92aeb15402f3af393466e5bc
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-02 17:41:30 +0200

    Create CommonValues.cs

---
+++ Polymorphism - Exercise/Vehicles/Common/CommonValues.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Vehicles
+{
+    public static class CommonValues
+    {
+        public const double ConsumptionIncreaseForCar = 0.9;
+        public const double ConsumptionIncreaseForTruck = 1.6;
+    }
+}
