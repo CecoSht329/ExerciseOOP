879d0d3fa1a6d323fc6e0cf167c66de6366f89c1
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-02 15:31:09 +0200

    Create StartUp.cs

---
+++ ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExplicitInterfaces
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                //PeshoPeshev Bulgaria 20
+                string name = tokens[0];
+                string country = tokens[1];
+                string age = tokens[2];
+                IPerson person = new Citizen(name, age, country);
+                IResident personAsResident = new Citizen(name, age, country);
+                Console.WriteLine(person.GetName());
+                Console.WriteLine(personAsResident.GetName());
+            }
+        }
+    }
+}
