a01cc53743c47ec19f660ebcbe7be46051fcb098
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-03 12:05:44 +0200

    Create BaseHero.cs

---
+++ Polymorphism - Exercise/Raiding/Contracts/BaseHero.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Reflection.Emit;
+
+namespace Raiding
+{
+    public abstract class BaseHero
+    {
+
+        public abstract string Name { get; }
+        public abstract int Power { get; }
+        public abstract string CastAbility();
+    }
+}
