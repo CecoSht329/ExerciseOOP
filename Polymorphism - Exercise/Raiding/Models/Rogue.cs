8857645a722d517b5465af137da0753311eb04a5
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-03 12:05:52 +0200

    Create Rogue.cs

---
+++ Polymorphism - Exercise/Raiding/Models/Rogue.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Raiding
+{
+    public class Rogue : BaseHero
+    {
+        private string name;
+        private readonly int power;
+        private const int roguePower = 80;
+        public Rogue(string name)
+        {
+            this.name = name;
+            power = roguePower;
+        }
+        public override string Name
+            => name;
+
+        public override int Power
+            => power;
+
+        public override string CastAbility()
+        {
+            return $"{nameof(Rogue)} - {Name} hit for {Power} damage";
+        }
+    }
+}
