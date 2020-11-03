03d0def72f1f9d4cd1d8d7c7784df3c4410f4f98
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-03 12:05:47 +0200

    Create Druid.cs

---
+++ Polymorphism - Exercise/Raiding/Models/Druid.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Raiding
+{
+    public class Druid : BaseHero
+    {
+        private string name;
+        private readonly int power;
+        private const int druidPower = 80;
+        public Druid(string name)
+        {
+            this.name = name;
+            this.power = druidPower;
+        }
+        public override string Name
+            => name;
+        public override int Power
+            => power;
+
+        public override string CastAbility()
+        {
+            return $"{nameof(Druid)} - {Name} healed for {Power}";
+        }
+    }
+}
