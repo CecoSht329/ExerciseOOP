cb03deefabea102948f3d627bf974708571ba756
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-03 12:05:55 +0200

    Create Warrior.cs

---
+++ Polymorphism - Exercise/Raiding/Models/Warrior.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Raiding
+{
+    public class Warrior : BaseHero
+    {
+        private string name;
+        private readonly int power;
+        private const int warriorPower = 100;
+        public Warrior(string name)
+        {
+            this.name = name;
+            power = warriorPower;
+        }
+        public override string Name
+            => name;
+
+        public override int Power
+            => power;
+        public override string CastAbility()
+        {
+            return $"{nameof(Warrior)} - {Name} hit for {Power} damage";
+        }
+    }
+}
