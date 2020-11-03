133abd5fb1ee542ffeb80b9137a67d785d7a49a4
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-03 11:11:04 +0200

    Update StartUp.cs

--- Polymorphism - Exercise/Vehicles/StartUp.cs
+++ Polymorphism - Exercise/Vehicles/StartUp.cs
@@ -8,11 +8,16 @@ namespace Vehicles
         {
             var carInfo = Console.ReadLine()
                 .Split(" ", StringSplitOptions.RemoveEmptyEntries);
-            Car car = new Car(double.Parse(carInfo[1]), double.Parse(carInfo[2]));
+            IVehicle car = new Car(double.Parse(carInfo[1]), double.Parse(carInfo[2]), double.Parse(carInfo[3]));
 
             var truckInfo = Console.ReadLine()
                 .Split(" ", StringSplitOptions.RemoveEmptyEntries);
-            Truck truck = new Truck(double.Parse(truckInfo[1]), double.Parse(truckInfo[2]));
+            IVehicle truck = new Truck(double.Parse(truckInfo[1]), double.Parse(truckInfo[2]), double.Parse(truckInfo[3]));
+
+            var busInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var bus = new Bus(double.Parse(busInfo[1]), double.Parse(busInfo[2]), double.Parse(busInfo[3]));
+
 
             var numberOfCommands = int.Parse(Console.ReadLine());
 
@@ -35,9 +40,22 @@ namespace Vehicles
                 {
                     truck.Refuel(double.Parse(command.Split(" ", StringSplitOptions.RemoveEmptyEntries)[2]));
                 }
+                else if (command.StartsWith("Refuel Bus"))
+                {
+                    bus.Refuel(double.Parse(command.Split(" ", StringSplitOptions.RemoveEmptyEntries)[2]));
+                }
+                else if (command.StartsWith("DriveEmpty Bus"))
+                {
+                    bus.Drive(double.Parse(command.Split(" ", StringSplitOptions.RemoveEmptyEntries)[2]));
+                }
+                else if (command.StartsWith("Drive Bus"))
+                {
+                    bus.DriveFull(double.Parse(command.Split(" ", StringSplitOptions.RemoveEmptyEntries)[2]));
+                }
             }
             Console.WriteLine($"Car: {car.FuelQuantity:f2}");
             Console.WriteLine($"Truck: {truck.FuelQuantity:f2}");
+            Console.WriteLine($"Bus: {bus.FuelQuantity:f2}");
         }
     }
 }
