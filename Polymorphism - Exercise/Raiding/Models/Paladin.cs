a3e777038477e3b018564e3bf95797b5bd7e527f
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-03 12:05:49 +0200

    Create Paladin.cs

---
+++ Polymorphism - Exercise/Raiding/Models/Paladin.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Raiding
+{
+    public class Paladin : BaseHero
+    {
+        private string name;
+        private readonly int power;
+        private const int paladinPower = 100;
+        public Paladin(string name)
+        {
+            this.name = name;
+            power = paladinPower;
+        }
+        public override string Name
+            => name;
+
+        public override int Power
+            => power;
+
+        public override string CastAbility()
+        {
+            return $"{nameof(Paladin)} - {Name} healed for {Power}";
+        }
+    }
+}
