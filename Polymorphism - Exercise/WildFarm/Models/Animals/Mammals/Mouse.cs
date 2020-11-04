b29e75f923ca9800fda9656d2fb62819d99dc166
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-04 15:54:41 +0200

    Create Mouse.cs

---
+++ Polymorphism - Exercise/WildFarm/Models/Animals/Mammals/Mouse.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+
+namespace WildFarm
+{
+    public class Mouse : Mammal
+    {
+        public Mouse(string name, double weight, string livingRegion)
+        {
+            Name = name;
+            Weight = weight;
+            LivingRegion = livingRegion;
+        }
+
+        public override string Name { get; protected set; }
+        public override double Weight { get; protected set; }
+        public override string LivingRegion { get; protected set; }
+        public override int FoodEaten { get; protected set; }
+
+        public override void Eat(Food food)
+        {
+            if ((food is Vegetable) || (food is Fruit))
+            {
+                FoodEaten += food.Quantity;
+                this.Weight += (CommonValues.MouseAfterFood * food.Quantity);
+            }
+            else
+            {
+                string noneatable = food.GetType().ToString().Split(".")[1];
+                throw new ArgumentException(string.Format(CommonValues.UnableToEatMessage, nameof(Mouse), noneatable));
+            }
+        }
+
+        public override string ProduceSound()
+        {
+            return "Squeak";
+        }
+    }
+}
