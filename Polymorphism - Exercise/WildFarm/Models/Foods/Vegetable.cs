3b4ba2457c3f00a30982a3368c0ed975f622d905
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-04 15:54:56 +0200

    Create Vegetable.cs

---
+++ Polymorphism - Exercise/WildFarm/Models/Foods/Vegetable.cs
@@ -0,0 +1,13 @@
+﻿
+namespace WildFarm
+{
+    public class Vegetable : Food
+    {
+        public Vegetable(int quantity)
+        {
+            Quantity = quantity;
+        }
+
+        public override int Quantity { get; protected set; }
+    }
+}
