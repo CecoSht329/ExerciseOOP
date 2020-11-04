cd406cdbf99386ead1e969e578b52ad4b01132ca
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-04 15:54:13 +0200

    Create Animal.cs

---
+++ Polymorphism - Exercise/WildFarm/Contracts/Animal.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace WildFarm
+{
+    public abstract class Animal
+    {
+        //•	Animal – string Name, double Weight, int FoodEaten;
+        public abstract string Name { get; protected set; }
+        public abstract double Weight { get; protected set; }
+        public abstract int FoodEaten { get; protected set; }
+
+        public abstract string ProduceSound();
+
+        public abstract void Eat(Food food);
+    }
+}
