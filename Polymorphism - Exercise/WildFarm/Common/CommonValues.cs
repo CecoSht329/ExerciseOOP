3ab8a6c9540fe1df171e1b1e08141e0b2485e5c6
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-04 15:54:10 +0200

    Create CommonValues.cs

---
+++ Polymorphism - Exercise/WildFarm/Common/CommonValues.cs
@@ -0,0 +1,22 @@
+﻿
+namespace WildFarm
+{
+    public static class CommonValues
+    {
+        //•	Hen – 0.35;
+        public const double HenAfterFood = 0.35;
+        //•	Owl – 0.25;
+        public const double OwlAfterFood = 0.25;
+        //•	Mouse – 0.10;
+        public const double MouseAfterFood = 0.10;
+        //•	Cat – 0.30;
+        public const double CatAfterFood = 0.30;
+        //•	Dog – 0.40;
+        public const double DogAfterFood = 0.40;
+        //•	Tiger – 1.00;
+        public const double TigerAfterFood = 1.00;
+
+        public const string UnableToEatMessage = "{0} does not eat {1}!";
+
+    }
+}
