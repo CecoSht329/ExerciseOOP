1b6902089ab073c0b671592aa9db5e1f530532d0
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-04 15:54:16 +0200

    Create Bird.cs

---
+++ Polymorphism - Exercise/WildFarm/Contracts/Bird.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace WildFarm
+{
+    public abstract class Bird : Animal
+    {
+        public abstract double WingSize { get; protected set; }
+
+        public override string ToString()
+        {
+            string birdType = this.GetType().ToString().Split(".")[1];
+            return $"{birdType} [{Name}, {WingSize}, {Weight}, {FoodEaten}]";
+        }
+    }
+}
