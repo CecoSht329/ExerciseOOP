0f086ff08a7f5e9e16ab0be63fd21c6f5208a2bd
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-04 15:54:34 +0200

    Create Owl.cs

---
+++ Polymorphism - Exercise/WildFarm/Models/Animals/Birds/Owl.cs
@@ -0,0 +1,72 @@
+﻿
+using System;
+
+namespace WildFarm
+{
+    public class Owl : Bird
+    {
+        private string name;
+        private double weight;
+        private double wingSize;
+        public Owl(string name, double weight, double wingSize)
+        {
+            Name = name;
+            Weight = weight;
+            WingSize = wingSize;
+        }
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            protected set
+            {
+                name = value;
+            }
+        }
+        public override double Weight
+        {
+            get
+            {
+                return weight;
+            }
+            protected set
+            {
+                weight = value;
+            }
+        }
+        public override double WingSize
+        {
+            get
+            {
+                return wingSize;
+            }
+            protected set
+            {
+                wingSize = value;
+            }
+        }
+
+        public override int FoodEaten { get; protected set; }
+
+        public override void Eat(Food food)
+        {
+            if (food is Meat)
+            {
+                FoodEaten += food.Quantity;
+                Weight += (CommonValues.OwlAfterFood * food.Quantity);
+            }
+            else
+            {
+                string noneatable = food.GetType().ToString().Split(".")[1];
+                throw new ArgumentException(string.Format(CommonValues.UnableToEatMessage, nameof(Owl), noneatable));
+            }
+        }
+
+        public override string ProduceSound()
+        {
+            return "Hoot Hoot";
+        }
+    }
+}
