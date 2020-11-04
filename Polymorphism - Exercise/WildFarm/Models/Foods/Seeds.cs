89b5652f9af01a6ce15584e946607101b0f7df67
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-04 15:54:50 +0200

    Create Seeds.cs

---
+++ Polymorphism - Exercise/WildFarm/Models/Foods/Seeds.cs
@@ -0,0 +1,13 @@
+﻿
+namespace WildFarm
+{
+    public class Seeds : Food
+    {
+        public Seeds(int quantity)
+        {
+            Quantity = quantity;
+        }
+
+        public override int Quantity { get; protected set; }
+    }
+}
