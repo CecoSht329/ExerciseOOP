3b498d9b384cd78d4c866e1c7ff13a177a09f1a1
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-04 15:54:44 +0200

    Create Tiger.cs

---
+++ Polymorphism - Exercise/WildFarm/Models/Animals/Mammals/Tiger.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+
+namespace WildFarm
+{
+    public class Tiger : Feline
+    {
+        public Tiger(string name, double weight, string livingRegion, string breed)
+        {
+            Name = name;
+            Weight = weight;
+            LivingRegion = livingRegion;
+            Breed = breed;
+        }
+
+        public override string Name { get; protected set; }
+        public override double Weight { get; protected set; }
+        public override string LivingRegion { get; protected set; }
+        public override string Breed { get; protected set; }
+        public override int FoodEaten { get; protected set; }
+
+        public override void Eat(Food food)
+        {
+            if (food is Meat)
+            {
+                FoodEaten += food.Quantity;
+                Weight += (CommonValues.TigerAfterFood * food.Quantity);
+            }
+            else
+            {
+                string noneatable = food.GetType().ToString().Split(".")[1];
+                throw new ArgumentException(string.Format(CommonValues.UnableToEatMessage, nameof(Tiger), noneatable));
+            }
+        }
+
+        public override string ProduceSound()
+        {
+            return "ROAR!!!";
+        }
+    }
+}
