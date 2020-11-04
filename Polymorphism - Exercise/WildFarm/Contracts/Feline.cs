5b84c1da5e4534aedf95fbb44a77768bdff97234
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-04 15:54:18 +0200

    Create Feline.cs

---
+++ Polymorphism - Exercise/WildFarm/Contracts/Feline.cs
@@ -0,0 +1,14 @@
+﻿
+namespace WildFarm
+{
+    public abstract class Feline : Mammal
+    {
+        public abstract string Breed { get; protected set; }
+
+        public override string ToString()
+        {
+            string felineType = this.GetType().ToString().Split(".")[1];
+            return $"{felineType} [{Name}, {Breed}, {Weight}, {LivingRegion}, {FoodEaten}]";
+        }
+    }
+}
