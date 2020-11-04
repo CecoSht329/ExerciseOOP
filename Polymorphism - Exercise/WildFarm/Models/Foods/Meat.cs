cecbcd52549a70711919d158ce326bb7c3d59bdf
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-04 15:54:48 +0200

    Create Meat.cs

---
+++ Polymorphism - Exercise/WildFarm/Models/Foods/Meat.cs
@@ -0,0 +1,13 @@
+﻿
+namespace WildFarm
+{
+    public class Meat : Food
+    {
+        public Meat(int quantity)
+        {
+            Quantity = quantity;
+        }
+
+        public override int Quantity { get; protected set; }
+    }
+}
