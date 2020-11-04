de831a32bf9f16715da86dfa382b0fa2302612e6
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-11-04 15:54:24 +0200

    Create Mammal.cs

---
+++ Polymorphism - Exercise/WildFarm/Contracts/Mammal.cs
@@ -0,0 +1,14 @@
+﻿
+namespace WildFarm
+{
+    public abstract class Mammal : Animal
+    {
+        public abstract string LivingRegion { get; protected set; }
+
+        public override string ToString()
+        {
+            string mammalType = this.GetType().ToString().Split(".")[1];
+            return $"{mammalType} [{Name}, {Weight}, {LivingRegion}, {FoodEaten}]";
+        }
+    }
+}
